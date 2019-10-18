a24fe0bbfcf5289b61dd3fdc0267ea7895566ad2
kubajj <vilouskubajj@gmail.com>
2019-10-19 00:07:10 +0100

    Turn turret with right indentation

--- bots/movement.py
+++ bots/movement.py
@@ -5,16 +5,16 @@ import random
 import math
 import logging
 class Movement:
-    gameServer : ServerComms = None
-    def __init__(self, gameServer):
-        self.gameServer = gameServer
+	gameServer : ServerComms = None
+	def __init__(self, gameServer):
+		self.gameServer = gameServer
 
-    def moveRight(self):
-        logging.info("Turning randomly")
-        #ss
-        self.gameServer.sendMessage(ServerMessageTypes.TURNTOHEADING, {'Amount': random.randint(0, 359)})
-        #self.gameServer.sendMessage(ServerMessageTypes.FIRE)
-    def turnTurret(self, angle):
-    	logging.info("Turning right to", angle)
-        #ss
-        self.gameServer.sendMessage(ServerMessageTypes.TURNTURRETTOHEADING, {'Amount': angle*360-180})
+	def moveRight(self):
+		logging.info("Turning randomly")
+		#ss
+		self.gameServer.sendMessage(ServerMessageTypes.TURNTOHEADING, {'Amount': random.randint(0, 359)})
+		#self.gameServer.sendMessage(ServerMessageTypes.FIRE)
+	def turnTurret(angle):
+		logging.info("Turning right to", angle)
+		#ss
+		self.gameServer.sendMessage(ServerMessageTypes.TURNTURRETTOHEADING, {'Amount': angle*360-180})
