eca0bf8bace858e6b2e07cff9c85f95f00bb8e66
kubajj <vilouskubajj@gmail.com>
2019-10-19 16:29:55 +0100

    info.py with converteddictionary

--- bots/info.py
+++ bots/info.py
@@ -1,117 +1,76 @@
 from communication import ServerComms
 from communication import ServerMessageTypes
+import numpy as np 
 
 import logging
 
 gameObjects = []
+convertedGameObjects = np.zeros(119)
 
 
 class InformationExtraction:
-    gameServer : ServerComms = None
+	gameServer : ServerComms = None
 
-    def __init__(self, gameServer):
-        self.gameServer = gameServer
+	def __init__(self, gameServer):
+		self.gameServer = gameServer
 
-    def readObjectUpdate(self):
-        message = self.gameServer.readMessage()
+	def readObjectUpdate(self):
+		message = self.gameServer.readMessage()
+		#18 means that it is a game object
+		if message['messageType'] == 18:
 
-        if message['messageType'] == 18:
+			# if the array is empty append first game object
+			if len(gameObjects) == 0:
+				gameObjects.append(message)
 
-            # if the array is empty append first game object
-            if len(gameObjects) == 0:
-                gameObjects.append(message)
+			else:
+				used = False
+				for i,dict in enumerate(gameObjects):
+					if dict['Id'] == message['Id']:
+						# update element
+						gameObjects[i] = message
+						used = True
+						break
+				if not used:
+					gameObjects.append(message)
+	def pasteObjectToConvertedGameObjects(self, convDict, index=0):
+		# paste the object into the empty spaces
+		for i in range(4):
+			if convDict[0] == convertedGameObjects[index + 7*i]:				
+				convertedGameObjects[index + i: index + (i + 1) * 7] = convDict
+				break
+			if convertedGameObjects[index + i * 7] == 0:
+				convertedGameObjects[index + i: index + (i + 1) * 7] = convDict
+				break    	
+	def convertGameObjects(self):
+		for gameObject in gameObjects:
+			convertedDictionary = []
 
-            else:
-                used = False
-                for i,dict in enumerate(gameObjects):
-                    if dict['Id'] == message['Id']:
-                        # update element
-                        gameObjects[i] = message
-                        used = True
-                        break
-                if not used:
-                    gameObjects.append(message)
+			for key in gameObject.keys():
+				if key == 'Id':
+					convertedDictionary.append( 1/ (-gameObject[key])) #just ids
+				elif key == 'X':    					
+					convertedDictionary.append((gameObject[key] + 70) / 140) #top is 1 and bottom is zero
+				elif key == 'Y':    					
+					convertedDictionary.append((gameObject[key] + 100) / 200) #left is 1 and right is zero
+				elif key == 'Heading':    					
+					convertedDictionary.append(((gameObject[key]/360)+0.5)%1) #0.5 == 0 degrees, 1 == 180 degrees, 0.25 = -90 degrees     
+				elif key == 'TurretHeading':  					
+					convertedDictionary.append(((gameObject[key]/360)+0.5)%1) #0.5 == 0 degrees, 1 == 180 degrees, 0.25 = -90 degrees     		
+				elif key == 'Health':
+					convertedDictionary.append(gameObject[key]/10)  					
+				elif key == 'Ammo':
+					convertedDictionary.append(gameObject[key]/10)  
 
-
-
-        print(message, type(message))
-
-
-
-class GameObject:
-
-    Id = None
-    Name = None
-    Type = None
-    X = None
-    Y = None
-    Heading = None
-    TurretHeading = None
-    Health = None
-    Ammo = None
-
-
-    def __init__self(self, dict):
-        self.Id = dict['Id']
-        self.Name = dict['Name']
-        self.Type = dict['Type']
-        self.X = dict['X']
-        self.Y = dict['Y']
-        self.Heading = dict['Heading']
-        self.TurretHeading = dict['TurretHeading']
-        self.Health = dict['Health']
-        self.Ammo = dict['Ammo']
-
-    def getId(self):
-        return self.Id
-
-    def getName(self):
-        return self.Name
-
-    def getType(self):
-        return self.Type
-
-    def getX(self):
-        return self.X
-
-    def getY(self):
-        return self.Y
-
-    def getCoords(self):
-        return (self.X, self.Y)
-
-    def getHeading(self):
-        return self.Heading
-
-    def getTurretHeading(self):
-        return self.TurretHeading
-
-    def getHealth(self):
-        return self.Health
-
-    def getAmmo(self):
-        return self.Ammo
-
-    def setX(self, x):
-        self.X = x
-
-    def setY(self, y):
-        self.Y = y
-
-    def setHeading(self, heading):
-        self.Heading = heading
-
-    def setTurretHeading(self, turretHeading):
-        self.TurretHeading = turretHeading
-
-    def setHealth(self, health):
-        self.Health = health
-
-    def setAmmo(self, ammo):
-        self.Ammo = ammo
-
-
-
-
-
-
+			if gameObject['Type'] == 'Tank':
+				if gameObject['Name'] == 'OurTeam:Bots':
+					pasteObjectToConvertedGameObjects(convertedDictionary)
+				else: #enemy tanks
+					pasteObjectToConvertedGameObjects(convertedDictionary,28)
+			elif gameObject['Type'] == 'AmmoPickup':
+					pasteObjectToConvertedGameObjects(convertedDictionary,56)
+			elif gameObject['Type'] == 'HealthPickup':
+					pasteObjectToConvertedGameObjects(convertedDictionary,84)
+			else:				
+				for i in range(7):
+					convertedGameObjects[112 + i: 112 + (i + 1) * 7] = convertedDictionary
