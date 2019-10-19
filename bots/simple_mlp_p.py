3594a6ab4d48b18648c5f382b843a9e54d1e6f74
ALLAN <allan.steven.pm@gmail.com>
2019-10-19 18:39:22 +0100

    update

--- bots/simple_mlp_p.py
+++ bots/simple_mlp_p.py
@@ -1,2 +1,43 @@
 import tensorflow as tf
 import numpy as np
+
+
+def mlp(x, sizes, activation=tf.tanh, output_activation=None):
+    # feedforward neural network.
+    for size in sizes[:-1]:
+        x = tf.layers.dense(x, units=size, activation=activation)
+    return tf.layers.dense(x, units=sizes[-1], activation=output_activation)
+
+def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, 
+          epochs=50, batch_size=5000, render=False):
+
+    # make environment, check spaces, get obs / act dims
+    """
+    env = gym.make(env_name)
+    assert isinstance(env.observation_space, Box), \
+        "This example only works for envs with continuous state spaces."
+    assert isinstance(env.action_space, Discrete), \
+        "This example only works for envs with discrete action spaces."
+	"""
+    obs_dim = (15,)#env.observation_space.shape[0] # 
+    n_acts = (7,)#env.action_space.n # q w e a s d and fire
+
+    # make core of policy network
+    obs_ph = tf.placeholder(shape=(None, obs_dim), dtype=tf.float32)
+    logits = mlp(obs_ph, sizes=hidden_sizes+[n_acts])
+
+    # make action selection op (outputs int actions, sampled from policy)
+    actions = tf.squeeze(tf.multinomial(logits=logits,num_samples=1), axis=1)
+
+    # make loss function whose gradient, for the right data, is policy gradient
+    weights_ph = tf.placeholder(shape=(None,), dtype=tf.float32)
+    act_ph = tf.placeholder(shape=(None,), dtype=tf.int32)
+    action_masks = tf.one_hot(act_ph, n_acts)
+    log_probs = tf.reduce_sum(action_masks * tf.nn.log_softmax(logits), axis=1)
+    loss = -tf.reduce_mean(weights_ph * log_probs)
+
+    # make train op
+    train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
