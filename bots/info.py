a435e86dbf9ba9190dab3550c0535e84e9350011
kubajj <vilouskubajj@gmail.com>
2019-10-19 21:32:11 +0100

    Info.py with getAllInfo

--- bots/info.py
+++ bots/info.py
@@ -19,6 +19,10 @@ class InformationExtraction:
 	def __init__(self, gameServer):
 		self.gameServer = gameServer
 
+	def getAllInfo(self):
+		self.readObjectUpdate()
+		self.convertedGameObjects()
+
 	def readObjectUpdate(self):
 		message = self.gameServer.readMessage()
 		#18 means that it is a game object
